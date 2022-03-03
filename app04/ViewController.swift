548e013d1cfeac935c6d3b9a2dd15c6266b15aa9
fengchenxiujisd <fengchenxiujisd@126.com>
2022-03-03 21:37:38 +0800

    初始化 test01

---
+++ app04/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app04
+//
+//  Created by zz on 2022/3/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var name:String?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
